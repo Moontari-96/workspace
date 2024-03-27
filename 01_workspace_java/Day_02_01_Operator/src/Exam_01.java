9d8326a81a720a989aadae6df1194e33fd8a9c80
Moontari <shaaa6256@gmail.com>
2024-03-27 09:56:04 +0900

    0327 연산자 과정 커밋

---
+++ 01_workspace_java/Day_02_01_Operator/src/Exam_01.java
@@ -0,0 +1,41 @@
+
+public class Exam_01 {
+	public static void main(String[] args) {
+		int num = 10; //	하나의 변수는 하나의 값만 가질 수 있다.
+		num = 20; //	num 변수에 저장되어 있던 10이 20에 의해서 덮어 씌어진다.
+				//	좌항은 반드시 변수여야만 한다.
+				//	변수의 이름은 중복될 수 없다.
+		int A = 10;
+		int B = 20;
+		
+//		A += B;	//	A = A + B;
+//				//	10 = 10 + 20; X
+//				//	A = 10 + 20; O
+//		System.out.println(A);	//	30
+//		System.out.println(B);	//	20
+		
+//		A -= B;
+//		System.out.println(A);	// -10
+//		System.out.println(B);	//	20
+		
+//		A *= B;
+//		System.out.println(A);	//	200
+//		System.out.println(B);	//	20
+		
+//		A = 10;
+//		B = 2;
+//		A /= B; // A = 10 / 2
+//		System.out.println(A);	//	5
+//		System.out.println(B);	//	2
+		
+//		A %= B; // A = 10 % 2; 
+//		System.out.println(A);	//	0
+//		System.out.println(B);	//	2				
+		
+//		A++;	//	후치형 증감연산자
+//		++A;	//	전치형 증감연산자
+		// A = A + 1;
+		System.out.println(A++);
+		System.out.println(A);
+	}
+}
